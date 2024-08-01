30ba1e8df8b293d64894f5d3a97c5e6be632e984
jayunandhu <jayunandhu26@gmail.com>
2024-08-01 16:39:24 -0400

    Header fix

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-* WEB700 – Assignment 05
+* WEB700 – Assignment 06
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
 * of this assignment has been copied manually or electronically from any other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Jaya Nandhini Kannan ID: 161496237 Date: 07/16/2024
+* Name: Jaya Nandhini Kannan ID: 161496237 Date: 8/1/2024
 *
 * Online (Verce;) Link: https://web700-app-nine.vercel.app/
 *
